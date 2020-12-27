dac51980ef0d990536c878e907623cb2b6a63b8b
wictor-parmenis <wictortec@gmail.com>
2020-12-27 10:20:40 -0300

    Update main page

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import path
+
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
